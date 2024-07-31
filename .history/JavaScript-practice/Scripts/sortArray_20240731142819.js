67fec3644249cda2299f1e0d48a9e5d4a279a224
TopJupeJupe <felixjoshua@gmail.com>
2024-07-31 14:36:47 +0530

    Minor Fixes

---
+++ .history/JavaScript-practice/Scripts/sortArray_20240731142819.js
@@ -0,0 +1,9 @@
+function sortArrayAndDisplay() {
+    let arr1 = [3, 8, 7, 6, 5, -4, 3, 2, 1];
+    arr1.sort((a, b) => a - b);
+    
+    // Display the sorted array in the document
+    document.getElementById('sortArrayOutput').textContent = 'Sorted Array: ' + arr1.join(', ');
+}
+
+document.getElementById('sortButton').addEventListener('click', sortArrayAndDisplay);
